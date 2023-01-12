cb31ed1533ed03514e3496a5f25437bee4fc84d2
paularolim <47821012+paularolim@users.noreply.github.com>
2023-01-11 22:43:32 -0300

    implement improves

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,34 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val nivel: Nivel, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val joe = Usuario()
+    val ana = Usuario()
+    
+    val conteudo1 = ConteudoEducacional("Kotlin básico", Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Kotlin funções", Nivel.INTERMEDIARIO, 60)
+    val conteudo3 = ConteudoEducacional("Kotlin orientado a objetos", Nivel.INTERMEDIARIO, 80)
+    val conteudo4 = ConteudoEducacional("Desafios com kotlin", Nivel.AVANCADO, 120)
+    
+    val formacao_conteudos = mutableListOf(conteudo1, conteudo2, conteudo3, conteudo4)
+    val formacao = Formacao("Kotlin experience", formacao_conteudos)
+    
+    formacao.matricular(joe)
+    formacao.matricular(ana)
+    
+    println(formacao.nome)
+    println(formacao.inscritos)
+    println(formacao.conteudos)
 }
